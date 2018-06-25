26e4cff3b6c2d5de74a6d6f2a14b1a810165d70a
Kwinten Pisman <kwinten.pisman@gmail.com>
2018-06-25 19:57:51 +0200

    chore: update versions to latests

--- src/app/mat-shared/mat-shared.module.ts
+++ src/app/mat-shared/mat-shared.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material';
+
+const angularMaterialModules = [
+    MatToolbarModule
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+    imports: [
+        CommonModule,
+        ...angularMaterialModules
+    ],
+    exports: [
+        ...angularMaterialModules
+    ],
+    declarations: []
 })
-export class MatSharedModule { }
+export class MatSharedModule {
+}
