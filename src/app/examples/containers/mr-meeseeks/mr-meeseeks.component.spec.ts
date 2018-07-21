f56ca97f723d6eaf153ec289548f60a083eec2e9
Kwinten Pisman <kwinten.pisman@gmail.com>
2018-07-21 15:10:39 +0200

    feat: add mr meeseeks container

---
+++ src/app/examples/containers/mr-meeseeks/mr-meeseeks.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MrMeeseeksComponent } from './mr-meeseeks.component';
+
+describe('MrMeeseeksComponent', () => {
+  let component: MrMeeseeksComponent;
+  let fixture: ComponentFixture<MrMeeseeksComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MrMeeseeksComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MrMeeseeksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/examples/containers/mr-meeseeks/mr-meeseeks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mr-meeseeks',
+  templateUrl: './mr-meeseeks.component.html',
+  styleUrls: ['./mr-meeseeks.component.css']
+})
+export class MrMeeseeksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
